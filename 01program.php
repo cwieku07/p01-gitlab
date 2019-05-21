4cef4729e5a33db4bc9010659f8c4c5d47b9050f
cwieku07 <35292833+cwieku07@users.noreply.github.com>
2019-05-21 09:09:10 +0200

    Piąty commit

--- 01program.php
+++ 01program.php
@@ -1,4 +1,8 @@
 <?php 
 echo ("Witaj");
+echo ("<br> jestem programem 01");
+echo ("<br>Test 11111");
+echo ("<br> TEST 22222");
+echo ("<br>Dalsza czesc programu");
+echo ("<br>Dalesza czesc programu 22222 z GitHuba");
 ?>
-
